2e492cde7c8d86b09aba536752f705c96c7f6ee3
little-csd <1140395168@qq.com>
2020-12-15 16:12:05 +0800

    draw info page complete

--- lib/config/constant.dart
+++ lib/config/constant.dart
@@ -1,25 +1,44 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 // 颜色相关的资源常量
 class ColorConstant {
-  static const backgroundGray = Color(0xfffafafa);
-  static const backgroundWhite = Color(0xffffffff);
-  static const borderGray = Color(0xffe6e6e6);
+  // 文本颜色常量
   static const textGray = Color(0xff999999);
+  static const textBlack = Colors.black;
+  static const textWhite = Colors.white;
+
+  // 前景色颜色常量
   static const primaryColor = Color(0xff7966FF);
   static const primaryColorLight = Color(0xFFFFDFFD);
+  static const purpleColor = Color(0xFF7966FF);
   static const accentColor = Color(0xff555555);
+  static const backgroundGray = Color(0xfffafafa);
+  static const backgroundWhite = Colors.white;
+
+  // 阴影相关颜色常量
+  static const borderGray = Color(0xffe6e6e6);
+  static const lightBorderPink = Color(0xFFF8F3FF);
 }
 
 // 字符串相关的资源常量
 class StringConstant {
-  static final info = '情报';
-  static final forum = '论坛';
-  static final home = '首页';
-  static final me = '我的';
-  static final edit = '编辑';
-  static final delete = '删除';
-  static final newComment = '最新评论';
+  // 首页常量
+  static const info = '正事';
+  static const forum = '放飞';
+  static const home = '首页';
+  static const me = '我的';
+  static const broadcast = '通知公告';
+  static const showReport = '查看 report';
+  static const projectTask = '项目任务';
+  static const freshmanTask = '新人任务';
+  static const fileData = '文件留存';
+  static const share = '交流分享';
+
+  static const edit = '编辑';
+  static const delete = '删除';
+  static const newComment = '最新评论';
 }
 
 class SvgIcon {
@@ -33,4 +52,5 @@ class SvgIcon {
   static final freshmanTask = _base + 'freshman_task.svg';
   static final projectTask = _base + 'project_task.svg';
   static final share = _base + 'share.svg';
+  static final broadcast = _base + 'broadcast.svg';
 }
---
+++ lib/tool/helper.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+Widget wrapPadding(Widget child, double vertical, double horizontal) {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: vertical, horizontal: horizontal),
+    child: child,
+  );
+}
+
+// 用于颜色的插值, 给定角度转化得到线性插值的起点+终点的 Alignment
+List<Alignment> transAngle2Alignments(double angle) {
+  angle = angle / 180 * pi;
+  final y = tan(angle);
+  Alignment start, end;
+  if (y > 1 || y < -1) {
+    start = Alignment(-1 / y, -1);
+    end = Alignment(1 / y, 1);
+  } else {
+    start = Alignment(-1, -y);
+    end = Alignment(1, y);
+  }
+  print(end);
+  return [start, end];
+}
