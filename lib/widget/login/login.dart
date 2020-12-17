0790ab46c45a394cdca98a533764934431c4b352
sssunki <skic6039@outlook.com>
2020-12-17 15:48:41 +0800

    bottom sheet base structure

--- lib/widget/login/login.dart
+++ lib/widget/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:UniqueBBSFlutter/config/constant.dart';
 import 'package:UniqueBBSFlutter/tool/helper.dart';
+import 'package:UniqueBBSFlutter/widget/common/NormalBottomSheetContainer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:UniqueBBSFlutter/widget/common/FilledTextField.dart';
@@ -33,33 +34,49 @@ class _LoginState extends State<LoginWidget> {
             Container(height: 63,),
             _buildLoginButton(),
             Container(height: 15,),
-            _buildWeComLoginButton(),
+            _buildWeComLoginButton(context),
           ],
         ),
       ),
     );
   }
 
-  // todo : delete test code
-  void textBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-        context: context,
-        backgroundColor: Colors.transparent,
-        builder: (BuildContext context) {
-          return Container(
-            height: 330,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(30),
-                topRight: const Radius.circular(30),
-              ),
-            ),
-            child: Text("circle"),
-          );
-        }
-    );
-  }
+}
+
+_buildLoginBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return _buildBottomSheetBody2(context);
+      }
+  );
+}
+
+_buildBottomSheetBody2(BuildContext context) {
+  return NormalBottomSheetContainer(
+    bottomCardRadius: 30,
+    totalHeight: 436,
+    pictureSrc: "images/no_phone_number.svg",
+    bottomSheetHeight: 294,
+    bottomSheetTopPadding: 55,
+    childInternal: Container(
+      padding: EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
+     child: Column(
+       crossAxisAlignment: CrossAxisAlignment.center,
+       children: [
+         Text("手机号未注册，\n用企业微信登录试试吧？"),
+         Container(height: 30,),
+         _buildIKnowButton(),
+         Container(height: 16,),
+         _buildWeComLoginButton(context),
+       ],
+     ),
+    )
+  );
 }
 
 _buildLoginButton() =>
@@ -80,7 +97,25 @@ _buildLoginButton() =>
       ),
     );
 
-_buildWeComLoginButton() =>
+_buildIKnowButton() =>
+    SizedBox(
+      width: double.infinity,
+      child: FlatButton(
+        height: 44,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0)),
+        color: ColorConstant.primaryColor,
+        child: Text(
+          "我知道了",
+          style: TextStyle(color: Colors.white, letterSpacing: 22),
+        ),
+        onPressed: () {
+          // todo: login internal
+        },
+      ),
+    );
+
+_buildWeComLoginButton(BuildContext context) =>
     SizedBox(
       width: double.infinity,
       child: FlatButton(
@@ -91,6 +126,7 @@ _buildWeComLoginButton() =>
         child: Text("使用企业微信扫码登录"),
         onPressed: () {
           // todo: route to WeCom Page
+          _buildLoginBottomSheet(context);
         },
       ),
     );
