e762b1c5623a67e496aaca51f5937fa9b8295cd3
sssunki <skic6039@outlook.com>
2021-01-03 21:08:57 +0800

    safe area

--- lib/widget/home/home.dart
+++ lib/widget/home/home.dart
@@ -100,34 +100,40 @@ class _HomeState extends State<HomeWidget> {
         _index = index;
       });
     };
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: _pages[_index],
-          ),
-          _buildMask(),
-          Container(
-            height: _bottomHeight,
-            width: double.infinity,
-            child: SvgPicture.asset(
-              SvgIcon.homeBottomBg,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          body: Stack(
+            alignment: Alignment.bottomCenter,
             children: [
-              _buildBottomItem(SvgIcon.message, 0, _index, tapIconCallback),
-              _buildBottomButton(context),
-              _buildBottomItem(SvgIcon.person, 1, _index, tapIconCallback),
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                child: _pages[_index],
+              ),
+              _buildMask(),
+              Container(
+                height: _bottomHeight,
+                width: double.infinity,
+                child: SvgPicture.asset(
+                  SvgIcon.homeBottomBg,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildBottomItem(SvgIcon.message, 0, _index, tapIconCallback),
+                  _buildBottomButton(context),
+                  _buildBottomItem(SvgIcon.person, 1, _index, tapIconCallback),
+                ],
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
